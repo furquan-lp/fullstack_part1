3b1e8a8d28859cdb1111edeff9cf7faa6f3130da
Syed Furquan Ahmad <furquan@pm.me>
2022-03-16 14:12:18 +0530

    1.6: unicafe step1

--- src/App.js
+++ src/App.js
@@ -1,58 +1,36 @@
-import React from 'react';
+import { useState } from 'react';
 
-const Header = (props) => {
+const MyButton = ({ text, handleClick }) => <button onClick={handleClick}>{text}</button>;
+
+const DisplayFeedback = ({ good, neutral, bad }) => {
   return (
-    <div>
-      <h1>{props.c.name}</h1>
-    </div>
-  )
+    <>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+    </>
+  );
 };
 
-const Content = (props) => {
-  return (
-    <div>
-      <p>{props.c.parts[0].name} {props.c.parts[0].exercises}</p>
-      <p>{props.c.parts[1].name} {props.c.parts[1].exercises}</p>
-      <p>{props.c.parts[2].name} {props.c.parts[2].exercises}</p>
-    </div>
-  )
-};
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises {props.c.parts[0].exercises + props.c.parts[1].exercises + props.c.parts[2].exercises}</p>
-    </div>
-  )
-};
-
-function App() {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  };
-  console.log(course);
+  const setState = (whom, value) => () => whom(value);
 
   return (
     <div>
-      <Header c={course} />
-      <Content c={course} />
-      <Total c={course} />
+      <h2>Give Feedback</h2>
+      <MyButton text='good' handleClick={setState(setGood, good + 1)} />
+      <MyButton text='neutral' handleClick={setState(setNeutral, neutral + 1)} />
+      <MyButton text='bad' handleClick={setState(setBad, bad + 1)} />
+      <h2>statistics:</h2>
+      <DisplayFeedback good={good} neutral={neutral} bad={bad} />
     </div>
   );
-}
+};
 
 export default App;
