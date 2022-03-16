557485417650c71fc8cea0ec080fb14bb1275f58
Syed Furquan Ahmad <furquan@pm.me>
2022-03-16 14:48:44 +0530

    1.12*: anecdotes step1

--- src/App.js
+++ src/App.js
@@ -1,41 +1,25 @@
 import { useState } from 'react';
 
-const Button = ({ text, handleClick }) => <button onClick={handleClick}>{text}</button>;
-
-const StatisticLine = ({ text, value }) => <tr><td>{text}</td> <td>{value}</td></tr>;
-
-const Statistics = ({ good, neutral, bad }) => {
-  const all = good + neutral + bad;
-  if (good === 0 && neutral === 0 && bad === 0)
-    return (<p>No feedback given.</p>);
-  return (
-    <table>
-      <StatisticLine text='good' value={good} />
-      <StatisticLine text='neutral' value={neutral} />
-      <StatisticLine text='bad' value={bad} />
-      <StatisticLine text='all' value={all} />
-      <StatisticLine text='average' value={(good - bad) / all} />
-      <StatisticLine text='positive' value={(good / all) * 100 + '%'} />
-    </table>
-  );
-};
+const MyButton = ({ text, handleClick }) => <button onClick={handleClick}>{text}</button>;
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ];
 
-  const setState = (whom, value) => () => whom(value);
+  const [selected, setSelected] = useState(0);
+  const random = (till) => Math.floor(Math.random() * till);;
 
   return (
     <div>
-      <h2>Give Feedback</h2>
-      <Button text='good' handleClick={setState(setGood, good + 1)} />
-      <Button text='neutral' handleClick={setState(setNeutral, neutral + 1)} />
-      <Button text='bad' handleClick={setState(setBad, bad + 1)} />
-      <h2>statistics:</h2>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      {anecdotes[selected]} <br />
+      <MyButton text='next anecdote' handleClick={() => setSelected(random(anecdotes.length))} />
     </div>
   );
 };
